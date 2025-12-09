1bc34c0502d8cae04e5dc5220b33ba614cde759f
Kelly Rojas <kellyyanethrojasmoreno@gmail.com>
2025-12-09 13:07:12 -0500

    ejercicio

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-  print("Hello learners!")
+import requests
+import json
 
-if __name__=="__main__":
-  main()
+def dish_fetch(numero):
+    
+    url = f"https://api-colombia.com/api/v1/TypicalDish/{numero}"
+    
+    response = requests.get(url)
+
+    # Convertir en diccionario
+    plato = response.json()
+
+    return plato
+
+
+if __name__ == "__main__":
+    numero = int(input("Ingrese el número del plato: "))
+    informacion_plato = dish_fetch(numero)
+    
+    print("Diccionario del plato:")
+    print(informacion_plato)
